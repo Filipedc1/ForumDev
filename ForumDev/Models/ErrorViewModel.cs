e165204f87d73ee0fe459809eb7ec30d96628d13
fcatarcione91 <fcatarcione91@gmail.com>
2018-11-10 13:04:05 -0500

    Add project files.

---
+++ ForumDev/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ForumDev.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
