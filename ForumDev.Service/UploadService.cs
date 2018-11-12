11bb7dc5b7b9e6a1ba5a3175a70ac253f20ddd8f
fcatarcione91 <fcatarcione91@gmail.com>
2018-11-12 11:55:33 -0500

    Register User, Upload services

--- ForumDev.Service/UploadService.cs
+++ ForumDev.Service/UploadService.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using ForumDev.Data;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace ForumDev.Service
 {
-    public class UploadService
+    public class UploadService : IUpload
     {
     }
 }
